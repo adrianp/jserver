d1338490525fc6aa4f30fe62aa26dd9acc7ffdbb
Adrian Tudor Panescu <adrian@panescu.com>
2012-10-29 17:32:32 +0100

    Revert "Added all required functionalities"
    This reverts commit a581de401cc90807f1ba6923acbf94fe3b2dbce6.

--- src/server/Config.java
+++ src/server/Config.java
@@ -10,23 +10,18 @@ package server;
 public final class Config {
 
 	/**
-	 * The port on which the server runs
+	 * 
 	 */
 	public static final int PORT = 8888;
 
 	/**
-	 * The path to where the documents served by the server are stored
+	 * 
 	 */
 	public static final String DOCUMENT_ROOT = "www";
 
 	/**
-	 * How many clients can we concurrently serve
+	 * 
 	 */
-	public static final int MAXIMUM_CLIENTS = 5;
-
-	/**
-	 * For how long should we keep connections alive
-	 */
-	public static final long KEEP_ALIVE = 5 * 1000;
+	public static final int MAXIMUM_CLIENTS = 3;
 
 }
